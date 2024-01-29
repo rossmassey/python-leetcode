8dd36b7a062e99b73042039e8f1a023aa35234b8
Ross Massey <massey0ross@gmail.com>
2024-01-28 18:33:13 -0800

    add list_node

---
+++ src/common/list_node.py
@@ -0,0 +1,9 @@
+"""
+Leetcode provided ListNode class
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
